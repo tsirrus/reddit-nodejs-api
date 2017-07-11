4edd7d0553011bc523bf79e6299ae2f783fe0e47
TSirrus <tsirrus@gmail.com>
2017-07-11 22:34:50 +0000

    Added subreddits and altered posts to add subredditsId

--- reddit.sql
+++ reddit.sql
@@ -21,3 +21,32 @@ CREATE TABLE posts (
   KEY userId (userId), -- why did we add this here? ask me :)
   CONSTRAINT validUser FOREIGN KEY (userId) REFERENCES users (id) ON DELETE SET NULL
 );
+
+-- Votes mapping table
+CREATE TABLE votes (
+  userId INT,
+  postId INT,
+  voteDirection TINYINT,
+  createdAt DATETIME NOT NULL,
+  updatedAt DATETIME NOT NULL,
+  PRIMARY KEY (userId, postId), -- this is called a composite key because it spans multiple columns. the combination userId/postId must be unique and uniquely identifies each row of this table.
+  KEY userId (userId), -- this is required for the foreign key
+  KEY postId (postId), -- this is required for the foreign key
+  FOREIGN KEY (userId) REFERENCES users (id) ON DELETE CASCADE, -- CASCADE means also delete the votes when a user is deleted
+  FOREIGN KEY (postId) REFERENCES posts (id) ON DELETE CASCADE -- CASCADE means also delete the votes when a post is deleted
+);
+
+-- Create Subreddits table
+CREATE TABLE subreddits (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  description VARCHAR(200) DEFAULT NULL,
+  createdAt DATETIME NOT NULL,
+  updatedAt DATETIME NOT NULL,
+  UNIQUE KEY name (name)
+);
+
+-- Add subredditsId to posts and foreign key constraint.
+ALTER TABLE posts 
+ADD COLUMN subredditsId INT,
+ADD CONSTRAINT validSubredditsId FOREIGN KEY (subredditsId) REFERENCES subreddits(id) ON DELETE CASCADE;
