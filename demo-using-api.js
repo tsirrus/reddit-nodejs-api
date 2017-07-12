86f3224ffb45ca53ea16ff1f2c37040a430c5664
TSirrus <tsirrus@gmail.com>
2017-07-12 17:27:55 +0000

    added connection-closing logic

--- demo-using-api.js
+++ demo-using-api.js
@@ -1,10 +1,11 @@
+'use strict';
 // load the mysql library
 var mysql = require('promise-mysql');
 
 // create a connection to our Cloud9 server
 var connection = mysql.createPool({
     host     : 'localhost',
-    user     : 'ziad_saab', // CHANGE THIS :)
+    user     : 'tsirrus',
     password : '',
     database: 'reddit',
     connectionLimit: 10
@@ -15,11 +16,12 @@ var RedditAPI = require('./reddit');
 
 var myReddit = new RedditAPI(connection);
 
+
 // We call this function to create a new user to test our API
 // The function will return the newly created user's ID in the callback
 myReddit.createUser({
-    username: 'PM_ME_CUTES',
-    password: 'abc123'
+    username: 'Test8',
+    password: 'Test8'
 })
     .then(newUserId => {
         // Now that we have a user ID, we can use it to create a new post
@@ -35,7 +37,14 @@ myReddit.createUser({
     .then(newPostId => {
         // If we reach that part of the code, then we have a new post. We can print the ID
         console.log('New post created! ID=' + newPostId);
+        //Return something?
+        //return true;
     })
+    //.then(myReddit.closeConnection(connection));
+    .then(function() {return connection.end()})
     .catch(error => {
         console.log(error.stack);
-    });
+        //Return something?
+        //return false;
+    })
+    .then(function() {return connection.end()});
